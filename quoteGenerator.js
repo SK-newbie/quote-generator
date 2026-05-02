58c8010402d70769167a2ed8048dd778e429c995
Sk-Newbie <oseagwinaexcellence@gmail.com>
2026-05-02 21:18:51 +0100

    c

---
+++ quoteGenerator.js
@@ -0,0 +1,101 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+const images = [
+  {
+    id: 1,
+    name: "Mountain View",
+    url: "https://picsum.photos/id/1018/600/400"
+  },
+  {
+    id: 2,
+    name: "Forest Path",
+    url: "https://picsum.photos/id/1015/600/400"
+  },
+  {
+    id: 3,
+    name: "City Skyline",
+    url: "https://picsum.photos/id/1011/600/400"
+  },
+  {
+    id: 4,
+    name: "Beach Sunset",
+    url: "https://picsum.photos/id/1003/600/400"
+  },
+  {
+    id: 5,
+    name: "Desert Dunes",
+    url: "https://picsum.photos/id/1002/600/400"
+  },
+  {
+    id: 6,
+    name: "Snowy Mountains",
+    url: "https://picsum.photos/id/1006/600/400"
+  },
+  {
+    id: 7,
+    name: "Waterfall",
+    url: "https://picsum.photos/id/1016/600/400"
+  },
+  {
+    id: 8,
+    name: "Night Sky",
+    url: "https://picsum.photos/id/1020/600/400"
+  },
+  {
+    id: 9,
+    name: "Bridge",
+    url: "https://picsum.photos/id/1024/600/400"
+  },
+  {
+    id: 10,
+    name: "Green Hills",
+    url: "https://picsum.photos/id/1025/600/400"
+  }
+];
+*/
